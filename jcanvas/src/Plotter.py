8aae157e8af43fd6912ec5462549ff1d8f133230
Jackie Chen <chen.jyr@gmail.com>
2013-10-02 22:28:58 -0700

    making magic happen

--- jcanvas/src/Plotter.py
+++ jcanvas/src/Plotter.py
@@ -1,12 +1,43 @@
 from Point import Point
 
 class Plotter:
-	
-	def __init__(self, initial_state):
-		self.state = initial_state
+    
+    def __init__(self, initial_state):
+        self.state = initial_state
 
-	def line_to(point):
-		pass
-	
-	def move_to(point):
-		pass
+    def line_to(self, point):
+        slope = self._calculate_slope(self.state, point)
+        points = self._generate_points(self.state, point, slope)
+        print "Points: "
+        for point in points:
+            print point.x, point.y
+        self._draw(points)
+    
+    def move_to(self, point):
+        self.state = point
+
+    def _calculate_slope(self, point_1, point_2):
+        y_diff = point_2.y - point_1.y
+        x_diff = point_2.x - point_1.x
+        gcd = self._gcd(y_diff, x_diff)
+        return (y_diff / gcd, x_diff / gcd)
+
+    def _generate_points(self, point_1, point_2, slope):
+        x_offset = 1 if point_2.x >= point_1.x else -1
+        y_offset = 1 if point_2.y >= point_1.y else -1
+        current_point = point_1
+        points = [current_point]
+        while current_point != point_2:
+            current_point = Point(current_point.x + slope[1], current_point.y + slope[0])
+            points.append(current_point)
+        return points
+
+    def _draw(self, points):
+        pass
+
+    def _gcd(self, a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+
--- jcanvas/src/Point.py
+++ jcanvas/src/Point.py
@@ -1,7 +1,18 @@
 
 class Point:
 
-	def __init__(self, x, y, z=0):
-		self.x = x
-		self.y = y
-		self.z = z
+    def __init__(self, x, y):
+        if x < 0 or y < 0:
+            raise Exception("Cannot be negative")
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
