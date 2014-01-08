31a5b5cc350e0be9f4abe04d0616ccd7ddbf77f9
Jonathan lafleche <arrow.69@gmail.com>
2014-01-08 14:53:17 -0800

    interactive shell/rasterizer/plotter emulator

---
+++ jcanvas/src2/interpreter.py
@@ -0,0 +1,76 @@
+import sys
+import rasterizer
+import softplotter
+
+def PrintHelpMessage():
+    print 'The grinch.ly drawing shell supports the following commands:'
+    print '\tellipse <w> <h>'
+    print '\tline <x> <y>'
+    print '\tmoveto <x> <y>'
+    print '\tshow'
+    print '\tquit'
+    print '\thelp'
+
+plotter = softplotter.SoftwarePlotter()
+
+def ParseCommand(command):
+    command = command.strip().split()
+    if len(command) < 1:
+        return True
+    if command[0] == 'ellipse':
+        print 'Coming Soon.'
+        return True
+
+        radius = int(command[0])
+        instructions = rasterizer.circle(radius)
+        plotter.ExecuteInstructions(instructions)
+    elif command[0] == 'moveto':
+        # get target position
+        dest_x = int(command[1])
+        dest_y = int(command[2])
+        # get current position
+        position = plotter.GetPosition()
+        curr_x = position[0]
+        curr_y = position[1]
+        # generate instructions
+        dx = dest_x - curr_x
+        dy = dest_y - curr_y
+        instructions = rasterizer.moveto(dx, dy)
+        # plot
+        plotter.ExecuteInstructions(instructions)
+    elif command[0] == 'line':
+        x = int(command[1])
+        y = int(command[2])
+        instructions = rasterizer.line(x,y)
+        plotter.ExecuteInstructions(instructions)
+    elif command[0] == 'show':
+        plotter.ShowPreview()
+    elif command[0] == 'quit':
+        print 'Goodbye!'
+        return False
+    elif command[0] == 'help':
+        PrintHelpMessage()
+    else:
+        print command[0], 'is not a supported command.'
+        PrintHelpMessage()
+    return True
+
+def RunScript(fname):
+    f = open(fname, 'r')
+    for line in f:
+        assert ParseCommand(line)
+    plotter.ShowPreview()
+
+if __name__ == '__main__':
+    
+    grinch_resource = open('./grinch.txt', 'r')
+    print grinch_resource.read()
+    print 'Welcome to the grinch.ly drawing shell.'
+
+
+    if len(sys.argv) == 2:
+        RunScript(sys.argv[1])
+    else:
+        while ParseCommand(raw_input()):
+            pass
+   
---
+++ jcanvas/src2/softplotter.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+
+#   Define Instruction Set as:
+#
+#   [0/1/2] [0/1/2] m       ## Move X/Y axis
+#   upz                     ## Pen Up
+#   dnz                     ## Pen Down
+
+def Move(value, direction):
+    if direction == '1':
+        return value + 1
+    elif direction == '2':
+        return value - 1
+    elif direction == '0':
+        return value
+    else:
+        assert False
+
+class SoftwarePlotter:
+    _pen_down = False
+    _xpos = 0
+    _ypos = 0
+    _xpts = []
+    _ypts = []
+
+    def ExecuteInstructions(self, instructions):
+        for instruct in instructions:
+            assert len(instruct) == 3
+            if instruct[2] == 'm':
+                old_x = self._xpos
+                old_y = self._ypos
+                self._xpos = Move(self._xpos, instruct[0])
+                self._ypos = Move(self._ypos, instruct[1])
+                if self._pen_down:
+                    self._xpts += [old_x, self._xpos]
+                    self._ypts += [old_y, self._ypos]
+            elif instruct[2] == 'z':
+                if instruct[:2] == 'up':
+                    self._pen_down = False
+                elif instruct[:2] == 'dn':
+                    self._pen_down = True
+                else:
+                    print instruct[:2]
+                    assert False
+            else:
+                assert False
+
+    def GetPosition(self):
+        return (self._xpos, self._ypos)
+
+    def ShowPreview(self):
+        plt.plot(self._xpts, self._ypts, color='black')
+        plt.show()
